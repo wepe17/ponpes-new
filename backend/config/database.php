af145a270c762da54bdfa49f6b777e554db7992b
Abizrh <abizarah857@gmail.com>
2025-01-24 22:26:27 +0700

    ✨ feat: re-structure folder

--- backend/config/database.php
+++ backend/config/database.php
@@ -2,8 +2,8 @@
 
 return [
     'host' => $_ENV['DB_HOST'] ?? 'localhost',
-    'username' => $_ENV['DB_USER'] ?? 'root',
-    'password' => $_ENV['DB_PASSWORD'] ?? '',
-    'database' => $_ENV['DB_NAME'] ?? 'pesantren_db',
+    'username' => $_ENV['DB_USER'] ?? 'bijhoo-dev',
+    'password' => $_ENV['DB_PASSWORD'] ?? 'bijhoo_dev',
+    'database' => $_ENV['DB_NAME'] ?? 'ponpes-db',
     'port' => $_ENV['DB_PORT'] ?? 3306,
 ];
